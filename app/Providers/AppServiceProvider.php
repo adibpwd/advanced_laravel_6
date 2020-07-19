ebc91772028ee6224b64319ba13aab0eac00c603
muslikhuladib0 <0adibcs0@gmail.com>
2020-07-19 21:54:39 +0700

    view::share, composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,10 @@ use App\Billing\BankPaymentGateway;
 use App\Billing\CreditPaymentGateway; 
 use App\Billing\PaymentGatewayContract;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use App\Channel;
+use App\Http\View\Composers\ChannelsComposer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -35,6 +39,22 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+        Schema::defaultStringLength(191);
+
+        // view::share('channels', Channel::orderBy('name', 'desc')->get());
+
+        // bintang artinya semua file yang ada difolder channel
+
+        // view::composer(['channel.*', 'post.create'], function ($view) {
+        //     $view->with('channels', Channel::orderBy('name', 'desc')->get());
+        // });
+
+        // bikin class di App\Http\Views\Composers\ChannelsComposer.php bikin method compose(View $view)
+        // isinya $view->with('channels', Channel::orderBy('name', 'desc')->get());
+        // terus bikin view::composer bisa bikin providers baru syaratnya narus nambahin serviceprovidernya ke config/app.php
+        // bisa pakai AppServiceProvider.php di method boot() kek dibawah ini..
+
+        // view::composer(['partials.channels.*'], ChannelsComposer::class);
+    
+        }
 }
